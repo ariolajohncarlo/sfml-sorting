a4c5a38989d6f6bce76d4d421180fd9693be3306
Carlo <ariola.johncarlo@gmail.com>
2023-04-28 21:51:52 +0800

    Fix cmake
    Fix cmake so that code compiles properly

--- sfml-sorting/sfml-sorting.cpp
+++ sfml-sorting/sfml-sorting.cpp
@@ -2,9 +2,27 @@
 //
 
 #include "sfml-sorting.h"
+#include <SFML/Graphics.hpp>
 
 int main()
 {
-	std::cout << "Hello CMake." << std::endl;
-	return 0;
+    sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
+    sf::CircleShape shape(100.f);
+    shape.setFillColor(sf::Color::Green);
+
+    while (window.isOpen())
+    {
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+            if (event.type == sf::Event::Closed)
+                window.close();
+        }
+
+        window.clear();
+        window.draw(shape);
+        window.display();
+    }
+
+    return 0;
 }
