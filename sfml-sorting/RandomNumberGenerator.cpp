e4d6e0c4c7608c61868b141ec6b47c874526d5cb
Carlo <ariola.johncarlo@gmail.com>
2023-04-29 16:13:55 +0800

    Add random number generator
    Add random number generator. Display 50 bars of differing heights next
    to each other.

---
+++ sfml-sorting/RandomNumberGenerator.cpp
@@ -0,0 +1,26 @@
+#include "RandomNumberGenerator.h"
+#include <cstdlib>
+#include <ctime>
+
+RandomNumberGenerator::RandomNumberGenerator(int min, int max)
+    : min(min)
+    , max(max)
+{
+    srand((unsigned) time(0));
+}
+
+int RandomNumberGenerator::GetMax() const
+{
+    return max;
+}
+
+int RandomNumberGenerator::GetMin() const
+{
+    return min;
+}
+
+int RandomNumberGenerator::GenerateRandomNumber() const
+{
+    auto numberGenerated = ((rand() % max) + min);
+    return static_cast<int>(numberGenerated);
+}
