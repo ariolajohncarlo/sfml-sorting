cae1016782e6816c5a6806ae9d0471291455d009
Carlo <ariola.johncarlo@gmail.com>
2023-05-21 17:18:46 +0800

    Setup directory for unit tests
    Setup directory for unit tests using the GTest framework.
    
    Add basic unit tests to check max and min values of
    RandomNumberGenerator class.

---
+++ tests/RandomNumberGeneratorTests.cpp
@@ -0,0 +1,12 @@
+#include <gtest/gtest.h>
+#include "../src/utils/RandomNumberGenerator.h"
+
+TEST(RandomNumberGeneratorTests, AssertCorrectMaxAndMinValues)
+{
+    const int MIN = 10;
+    const int MAX = 100;
+    RandomNumberGenerator randomNumberGenerator{MIN, MAX};
+
+    EXPECT_EQ(randomNumberGenerator.GetMin(), MIN);
+    EXPECT_EQ(randomNumberGenerator.GetMax(), MAX);
+}
